4110a3bfa67f530ad4654891cdc1a78ab8186b3a
alivemary <alivemary@gmail.com>
2017-03-03 10:18:17 +0100

    react projects added

--- js/portfolio.js
+++ js/portfolio.js
@@ -6,14 +6,22 @@
             case 'aM':
                 $('#aboutMe').removeClass('hidden');
                 $('#projects').removeClass('hidden');
+                $('#react').removeClass('hidden');
                 break;
             case 'pR':
                 $('#aboutMe').addClass('hidden');
                 $('#projects').removeClass('hidden');
+                $('#react').addClass('hidden');
+                break;
+            case 'rP':
+                $('#aboutMe').addClass('hidden');
+                $('#projects').addClass('hidden');
+                $('#react').removeClass('hidden');
                 break;
             case 'last':
                 $('#aboutMe').removeClass('hidden');
                 $('#projects').addClass('hidden');
+                $('#react').addClass('hidden');
                 break;
         }
     });
