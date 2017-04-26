909ca915832a25a2de94d67a94ee0c6414464c14
alivemary <alivemary@gmail.com>
2017-04-26 09:13:03 +0200

    dungeon game added

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,28 +1,30 @@
-﻿$(document).ready(function () {
-   
-    $('.nav-item').on('click', function () {
-        console.log(this.id);
-        switch (this.id) {
-            case 'aM':
-                $('#aboutMe').removeClass('hidden');
-                $('#projects').removeClass('hidden');
-                $('#react').removeClass('hidden');
-                break;
-            case 'pR':
-                $('#aboutMe').addClass('hidden');
-                $('#projects').removeClass('hidden');
-                $('#react').addClass('hidden');
-                break;
-            case 'rP':
-                $('#aboutMe').addClass('hidden');
-                $('#projects').addClass('hidden');
-                $('#react').removeClass('hidden');
-                break;
-            case 'last':
-                $('#aboutMe').removeClass('hidden');
-                $('#projects').addClass('hidden');
-                $('#react').addClass('hidden');
-                break;
-        }
-    });
+﻿$(document).ready(function() {
+  $(".nav-item").on("click", function() {
+    console.log(this.id);
+    switch (this.id) {
+      case "aM":
+        $("#aboutMe").removeClass("hidden");
+        $("#projects").removeClass("hidden");
+        $("#react").removeClass("hidden");
+        break;
+
+      case "pR":
+        $("#aboutMe").addClass("hidden");
+        $("#projects").removeClass("hidden");
+        $("#react").addClass("hidden");
+        break;
+
+      case "rP":
+        $("#aboutMe").addClass("hidden");
+        $("#projects").addClass("hidden");
+        $("#react").removeClass("hidden");
+        break;
+
+      case "last":
+        $("#aboutMe").removeClass("hidden");
+        $("#projects").addClass("hidden");
+        $("#react").addClass("hidden");
+        break;
+    }
+  });
 });
