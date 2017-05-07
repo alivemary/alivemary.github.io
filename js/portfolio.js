26bc734b1bf4e4f4b5952e7dbfcaf635135ec6ff
alivemary <alivemary@gmail.com>
2017-05-08 00:01:05 +0200

    webshop added

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,29 +1,39 @@
 ﻿$(document).ready(function() {
   $(".nav-item").on("click", function() {
-    console.log(this.id);
     switch (this.id) {
       case "aM":
         $("#aboutMe").removeClass("hidden");
         $("#projects").removeClass("hidden");
         $("#react").removeClass("hidden");
+        $("#webshop").removeClass("hidden");
         break;
 
       case "pR":
         $("#aboutMe").addClass("hidden");
         $("#projects").removeClass("hidden");
         $("#react").addClass("hidden");
+        $("#webshop").addClass("hidden");
         break;
 
       case "rP":
         $("#aboutMe").addClass("hidden");
         $("#projects").addClass("hidden");
         $("#react").removeClass("hidden");
+        $("#webshop").addClass("hidden");
+        break;
+
+       case "wS":
+        $("#aboutMe").addClass("hidden");
+        $("#projects").addClass("hidden");
+        $("#react").addClass("hidden");
+        $("#webshop").removeClass("hidden");
         break;
 
       case "last":
         $("#aboutMe").removeClass("hidden");
         $("#projects").addClass("hidden");
         $("#react").addClass("hidden");
+        $("#webshop").addClass("hidden");
         break;
     }
   });
